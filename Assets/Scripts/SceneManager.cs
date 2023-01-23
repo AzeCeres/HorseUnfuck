1fdd167703989a281babf0fd92c35d1dc599e72c
Tmmrvg <Oetom01@outlook.com>
2023-01-23 13:22:50 +0100

    SceneManager and folders

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    private bool gameOver = false;
+    public GameObject gameOverMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver == true)
+        {
+            gameOverMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void Restart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        print("The game is restarted");
+        gameOverMenu.SetActive(false);
+    }
+}
